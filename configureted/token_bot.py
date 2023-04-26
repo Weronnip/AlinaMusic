ffed816018dbe3ece0825d6d7ffd1d48d8e19814
Alexey Kuriganov <115900783+Weronnip@users.noreply.github.com>
2023-04-26 14:19:18 +0300

    Update token_bot.py

--- configureted/token_bot.py
+++ configureted/token_bot.py
@@ -1,2 +1,2 @@
-token_bot = "6026396073:AAESiSYFWYm-sF3HZSKw0g638a48Fw4EnL4"
-yandex_token = "y0_AgAAAAAulFtJAAG8XgAAAADhmroFuajHbwhVRUGXTx8SQg-iOweTVBI&token_type=bearer&expires_in=31273359"
+token_bot = "6026396*********************"
+yandex_token = "*******************************************"
